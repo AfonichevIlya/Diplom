933f5053b8e67ce0c44790578539679489268740
AfonichevIlya <afonichev007@gmail.com>
2024-05-15 00:21:49 +0300

    commit

--- models/entry.js
+++ models/entry.js
@@ -1,5 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("test.db");
+const db = new sqlite3.Database("test.sqlite");
 
 const createTableSQL = `
   CREATE TABLE IF NOT EXISTS entries (
